35e201b512732022cd8236cf0a89db2ab5f882b4
liyo86 <77936857+liyo86@users.noreply.github.com>
2021-01-24 20:35:42 +0100

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Renderer fondo; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     fondo.material.mainTextureOffset =  fondo.material.mainTextureOffset + new Vector2(0.020f, 0) * Time.deltaTime;
+    }
+}
